cc20d1f90cd228d6287518166e386125765e3b83
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-24 18:50:38 -0400

    save features and labels to store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
+import * as nifti from 'nifti/src';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -5,12 +6,45 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    featuresArrayBuffer: new ArrayBuffer(0),
+    predictionsArray: new Uint8Array(0),
   },
   mutations: {
-
+    SET_VOLUME_FILE_ARRAY_BUFFER(state, event: Event) {
+      if (event.target !== null) {
+        const input = event.target as HTMLInputElement;
+        const files = input.files as FileList;
+        if (files !== null && files[0]) {
+          const reader = new FileReader();
+          reader.onload = (e: ProgressEvent) => {
+            if (e.target !== null) {
+              state.featuresArrayBuffer = reader.result as ArrayBuffer;
+            } else {
+              throw Error('Error loading volume file.');
+            }
+          };
+          reader.readAsArrayBuffer(files[0]);
+        }
+      }
+    },
+    SET_PREDICTED_VOLUME(state, data: Uint8Array) {
+      state.predictionsArray = data;
+    },
   },
-  actions: {
+
+  getters: {
+    featuresNifti(state): nifti.Image {
+      if (state.featuresArrayBuffer.byteLength === 0) {
+        throw Error('Volume array is empty. Was the volume file loaded?');
+      }
+      return nifti.fromBuffer(state.featuresArrayBuffer);
+    },
+    predictionsArray(state): Uint8Array {
+      if (state.predictionsArray.byteLength === 0) {
+        throw Error('Prediction array is empty.');
+      }
+      return state.predictionsArray;
+    },
 
   },
 });
