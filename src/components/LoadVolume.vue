fb298becccedf0f99a91a4fad1d1f31a4f010363
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-24 10:14:19 -0400

    add loadvolume

---
+++ src/components/LoadVolume.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello"></div>
+</template>
+
+<script lang="ts">
+import * as nifti from 'nifti/src';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoadVolume extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
